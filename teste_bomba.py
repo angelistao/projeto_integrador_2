504a5e9ccbf9f98c1345c10989c0266c0d00258d
angelistao <angel.mahlke@gmail.com>
2026-06-25 17:26:19 -0300

    [FIX] rele pin

--- teste_bomba.py
+++ teste_bomba.py
@@ -4,20 +4,19 @@ import time
 # Define o modo de numeração dos pinos como BCM (como no seu main_control.py)
 GPIO.setmode(GPIO.BCM)
 
-# Configura o pino 17 como saída (pino do relé da bomba)
-PINO_RELE_BOMBA = 17
+PINO_RELE_BOMBA = 11
 GPIO.setup(PINO_RELE_BOMBA, GPIO.OUT)
 
 try:
     print("--- INICIANDO TESTE DO RELÉ ---")
     
-    print("Enviando sinal ALTO (HIGH) para o pino 17... [Bomba deve LIGAR]")
+    print("Enviando sinal ALTO (HIGH) para o pino 11... [Bomba deve LIGAR]")
     GPIO.output(PINO_RELE_BOMBA, GPIO.HIGH)
     
     # Mantém ligado por 3 segundos para teste seguro
     time.sleep(3)
     
-    print("Enviando sinal BAIXO (LOW) para o pino 17... [Bomba deve DESLIGAR]")
+    print("Enviando sinal BAIXO (LOW) para o pino 11... [Bomba deve DESLIGAR]")
     GPIO.output(PINO_RELE_BOMBA, GPIO.LOW)
     
     print("--- TESTE CONCLUÍDO COM SUCESSO ---")
